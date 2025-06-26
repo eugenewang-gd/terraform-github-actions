cfbc8201fb81cee77277571f580e4e8a8aeb48ee
Eugene Wang <eugene.wang@greendotcorp.com>
2025-06-26 17:24:02 +0800

    blob update

--- main.tf
+++ main.tf
@@ -8,17 +8,17 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
+    resource_group_name  = "shengchang-test"
+    storage_account_name = "shengchangpoctest"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
+    use_oidc             = false
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
+  use_oidc = false
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
